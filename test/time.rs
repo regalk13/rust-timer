7a9947d5fbc13ef8544bc869627f6223e15246bb
Regalk <jsalazarcaviedes30@gmail.com>
2023-01-04 08:02:19 -0500

    Add converter of time

--- test/time.rs
+++ test/time.rs
@@ -1,20 +1,17 @@
+fn convert_time(mut seconds: u32) -> (u32, u32, u32) {
+    let hour = seconds / 3600;
+    seconds = seconds % 3600;
+    let minutes = seconds / 60;
+    seconds = seconds % 60;
+    (hour, minutes, seconds)
+}
 fn main() {
     const DELTA_TIME: f64 = 1.0 / 60.0;
-    let seconds = 2.0;
-    let minutes = 5.0;
-
-    let mut result = 0.0;
-    result += seconds;
-    result += minutes * 60.0;
-    loop {
-        let minutes = result / 60.0 % 60.0;
-        println!("{}:{}", minutes / 10.0, minutes % 10.0);
-        if result > 1e-6 {
-            result -= DELTA_TIME;
-        } else {
-            result = 0.0;
-            println!("We end!");
-            break;
-        }
-    }
+    let mut result = 12345;
+    let time = convert_time(result);
+    println!("{}:{}:{}", time.0, time.1, time.2);
+    // Add a minute
+    result += 60;
+    let time = convert_time(result);
+    println!("{}:{}:{}", time.0, time.1, time.2);
 }
