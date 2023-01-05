5e22e4026c4221959cdb45ef13981e3dc651aaae
Regalk <jsalazarcaviedes30@gmail.com>
2023-01-05 17:15:19 -0500

    Rendering the time

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::{Texture, TextureCreator, TextureQuery};
 use sdl2::video::{Window, WindowContext};
+static SCREEN_WIDTH: u32 = 800;
+static SCREEN_HEIGHT: u32 = 600;
 
+macro_rules! rect(
+    ($x:expr, $y:expr, $w:expr, $h:expr) => (
+        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+    )
+);
 mod timer {
     #[derive(Copy, Clone, Default)]
     pub enum State {
@@ -38,16 +45,42 @@ mod timer {
             seconds = seconds % 60;
             (hour, minutes, seconds)
         }
+        pub fn update(&mut self) {}
     }
 }
+fn get_centered_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32) -> Rect {
+    let wr = rect_width as f32 / cons_width as f32;
+    let hr = rect_height as f32 / cons_height as f32;
 
+    let (w, h) = if wr > 1f32 || hr > 1f32 {
+        if wr > hr {
+            println!("Scaling down! The text will look worse!");
+            let h = (rect_height as f32 / wr) as i32;
+            (cons_width as i32, h)
+        } else {
+            println!("Scaling down! The text will look worse!");
+            let w = (rect_width as f32 / hr) as i32;
+            (w, cons_height as i32)
+        }
+    } else {
+        (rect_width as i32, rect_height as i32)
+    };
+
+    let cx = (SCREEN_WIDTH as i32 - w) / 2;
+    let cy = (SCREEN_HEIGHT as i32 - h) / 2;
+    rect!(cx, cy, w, h)
+}
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     const DELTA_TIME: f64 = 1.0 / 60.0;
+    let args: Vec<_> = env::args().collect();
+    println!("linked sdl2_ttf: {}", sdl2::ttf::get_linked_version());
+    let path = Path::new(&args[1]);
 
     let window = video_subsystem
-        .window("Timer", 800, 600)
+        .window("Timer", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
@@ -59,6 +92,7 @@ pub fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
+    let texture_creator = canvas.texture_creator();
     println!("Rendering the timer with \"{}\"", canvas.info().name);
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
@@ -87,14 +121,40 @@ pub fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        let actual_time = timer.get_time();
 
-        println!("{}:{}:{}", actual_time.0, actual_time.1, actual_time.2);
+        let actual_time = timer.get_time();
         timer.seconds += 1;
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
+
         let seconds = time::Duration::from_secs((1.0 * 60.0 * DELTA_TIME) as u64);
+
+        let mut font = ttf_context.load_font(path, 128)?;
+        font.set_style(sdl2::ttf::FontStyle::NORMAL);
+        let surface = font
+            .render(&format!(
+                "{}:{}:{}",
+                actual_time.0, actual_time.1, actual_time.2
+            ))
+            .blended(Color::RGBA(255, 255, 255, 255))
+            .map_err(|e| e.to_string())?;
+        let texture = texture_creator
+            .create_texture_from_surface(&surface)
+            .map_err(|e| e.to_string())?;
+        let TextureQuery { width, height, .. } = texture.query();
+        // If the example text is too big for the screen, downscale it (and center irregardless)
+        let padding = 64;
+        let target = get_centered_rect(
+            width,
+            height,
+            SCREEN_WIDTH - padding,
+            SCREEN_HEIGHT - padding,
+        );
+        canvas.copy(&texture, None, Some(target))?;
+        canvas.present();
+        //canvas.copy()
+
         thread::sleep(seconds);
     }
 
