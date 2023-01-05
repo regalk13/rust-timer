97ade702a02a92df2d077a7114f30e43cf68296f
Regalk <jsalazarcaviedes30@gmail.com>
2023-01-05 09:09:19 -0500

    Add delta time to the timer

--- test/time.rs
+++ test/time.rs
@@ -1,3 +1,5 @@
+use std::{thread, time};
+
 fn convert_time(mut seconds: u32) -> (u32, u32, u32) {
     let hour = seconds / 3600;
     seconds = seconds % 3600;
@@ -7,11 +9,16 @@ fn convert_time(mut seconds: u32) -> (u32, u32, u32) {
 }
 fn main() {
     const DELTA_TIME: f64 = 1.0 / 60.0;
+
     let mut result = 12345;
-    let time = convert_time(result);
-    println!("{}:{}:{}", time.0, time.1, time.2);
-    // Add a minute
-    result += 60;
-    let time = convert_time(result);
-    println!("{}:{}:{}", time.0, time.1, time.2);
+    loop {
+        let time = convert_time(result);
+        println!("{}:{}:{}", time.0, time.1, time.2);
+        println!("The delta time is: {}", DELTA_TIME);
+
+        // Add a second
+        result += 1;
+        let ten_millis = time::Duration::from_secs((1.0 * 60.0 * DELTA_TIME) as u64);
+        thread::sleep(ten_millis);
+    }
 }
